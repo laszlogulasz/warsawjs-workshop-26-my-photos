5c34ac42c294da73af7e858d3158a25a43bebfc9
Michał Krul <michal@krul.pl>
2018-11-18 17:25:01 +0100

    working ;)

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ import Image from "/image.js";
 
 const images = [image1, image2, image3];
 const favButton = document.getElementById("show-favourites");
+const fileInput = document.getElementById("file-input");
 const imagesList = images.map(path => new Image(path));
 
 imagesList.forEach(image => image.show());
-favButton.addEventListener("click", () =>
-  imagesList.filter(image => image.isFavorite()).forEach(image => image.hide())
-);
+favButton.addEventListener("click", () => {
+  imagesList.filter(image => image.isFavorite()).forEach(image => image.hide());
+});
+
+fileInput.onchange = () => {
+  if (fileInput.files && fileInput.files[0]) {
+    const reader = new FileReader();
+    reader.onload = e => {
+      const newEl = new Image(e.target.result).show();
+      imagesList.push(newEl);
+    };
+
+    reader.readAsDataURL(fileInput.files[0]);
+  }
+};
