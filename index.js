a24570840a00e9c6776dcb7f03640dc97966f2c4
Michał Krul <michal@krul.pl>
2018-11-18 14:13:25 +0100

    added class

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('warsawJS workshop');
+const images = ["confront.png", "warsawjs.png"];
+const imagesGallery = document.getElementById("images");
+
+class Image {
+  constructor(path) {
+    this.path = path;
+  }
+
+  show() {
+    this.imageElement = document.createElement("img");
+    this.imageElement.setAttribute("src", `images/${this.path}`);
+    this.imageElement.addEventListener("click", () => this.toggleFavorite());
+    imagesGallery.appendChild(this.imageElement);
+  }
+
+  toggleFavorite() {
+    this.imageElement.classList.toggle("image--favourite");
+  }
+}
+
+images.forEach(fileName => {
+  const image = new Image(fileName);
+  image.show();
+});
